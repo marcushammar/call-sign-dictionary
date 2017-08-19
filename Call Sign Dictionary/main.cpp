07c19e69c3dab7aaa10400411c934dc1972d33c3
Marcus Hammar <marcus@infium.se>
2017-08-19 23:13:14 +0200

    Add questions to collect the basic data from the user

--- Call Sign Dictionary/main.cpp
+++ Call Sign Dictionary/main.cpp
@@ -1,5 +1,16 @@
 #include <iostream>
 
+using namespace std;
+
 int main(int argc, const char * argv[]) {
+    string dictionary, prefix;
+    int length;
+    cout << "What dictionary file would you like to use?" << endl;
+    cin >> dictionary;
+    cout << "What call sign prefix would you like?" << endl;
+    cin >> prefix;
+    cout << "How long should the call sign be?" << endl;
+    cin >> length;
+    cout << "You want to generate a call sign from the file " << dictionary << " that is " << length << " characters long and has the prefix " << prefix << "." << endl;
     return 0;
 }
