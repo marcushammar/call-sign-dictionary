14e9fc7eb3880b5f60794d11736600e1e4cccb41
Marcus Hammar <marcus@infium.se>
2017-08-19 22:51:38 +0200

    Initial Commit

---
+++ Call Sign Dictionary/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Call Sign Dictionary
+//
+//  Created by Marcus Hammar on 2017-08-19.
+//  Copyright © 2017 Marcus Hammar. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
